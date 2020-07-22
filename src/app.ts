15382f4c73e4e36f7738461da80329aeec6cb6cb
Justin <justin@rsad.co.kr>
2020-07-22 18:10:45 +0900

    more on type guards

--- src/app.ts
+++ src/app.ts
@@ -14,9 +14,60 @@ const e1: ElevatedEmployee = {
     privileges: ['create-server'],
     startDate: new Date
 }
-console.log(e1);
+// console.log(e1);
 
-type Combine = string
+type Combine = string | number
 type Numberic = number | boolean
 
 type Universal = Combine & Numberic
+
+let add = (a: Combine, b: Combine) => {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+
+type UnknownEmployee = Employee | Admin
+
+let printEmployeeInformation = (emp: UnknownEmployee) => {
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('startDate:', emp.startDate);
+    }
+    console.log('Name: ' + emp.name);
+}
+
+printEmployeeInformation(e1)
+
+class Car {
+    drive() {
+        console.log('driving');
+    }
+}
+class Truck {
+    drive() {
+        console.log('driving a truck');
+    }
+
+    loadCargo(amount: number) {
+        console.log('loading cargo ' + amount);
+    }
+}
+
+type Vehicle = Car | Truck 
+
+const v1 = new Car()
+const v2 = new Truck()
+
+let useVehicle = (vehicle: Vehicle) => {
+    vehicle.drive()
+    if ('loadCargo' in vehicle) {
+        vehicle.loadCargo(1000)
+    }
+}
+
+useVehicle(v1)
+useVehicle(v2)
