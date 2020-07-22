7a1eb2993cf211466f3c3b0c65ffe0607e6cf465
Justin <justin@rsad.co.kr>
2020-07-22 18:20:20 +0900

    discriminated unions

--- src/app.ts
+++ src/app.ts
@@ -1,73 +1,97 @@
 type Admin = {
-    name: string
-    privileges: string[]
-}
+  name: string;
+  privileges: string[];
+};
 
 type Employee = {
-    name: string
-    startDate: Date
-}
+  name: string;
+  startDate: Date;
+};
 
-type ElevatedEmployee = Admin & Employee
+type ElevatedEmployee = Admin & Employee;
 const e1: ElevatedEmployee = {
-    name: "max", 
-    privileges: ['create-server'],
-    startDate: new Date
-}
+  name: "max",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
 // console.log(e1);
 
-type Combine = string | number
-type Numberic = number | boolean
+type Combine = string | number;
+type Numberic = number | boolean;
 
-type Universal = Combine & Numberic
+type Universal = Combine & Numberic;
 
 let add = (a: Combine, b: Combine) => {
-    if (typeof a === 'string' || typeof b === 'string') {
-        return a.toString() + b.toString();
-    }
-    return a + b;
-}
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+};
 
-type UnknownEmployee = Employee | Admin
+type UnknownEmployee = Employee | Admin;
 
 let printEmployeeInformation = (emp: UnknownEmployee) => {
-    if ('privileges' in emp) {
-        console.log('Privileges: ' + emp.privileges);
-    }
-    if ('startDate' in emp) {
-        console.log('startDate:', emp.startDate);
-    }
-    console.log('Name: ' + emp.name);
-}
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("startDate:", emp.startDate);
+  }
+  console.log("Name: " + emp.name);
+};
 
-printEmployeeInformation(e1)
+// printEmployeeInformation(e1)
 
 class Car {
-    drive() {
-        console.log('driving');
-    }
+  drive() {
+    console.log("driving");
+  }
 }
 class Truck {
-    drive() {
-        console.log('driving a truck');
-    }
+  drive() {
+    console.log("driving a truck");
+  }
 
-    loadCargo(amount: number) {
-        console.log('loading cargo ' + amount);
-    }
+  loadCargo(amount: number) {
+    console.log("loading cargo " + amount);
+  }
 }
 
-type Vehicle = Car | Truck 
+type Vehicle = Car | Truck;
 
-const v1 = new Car()
-const v2 = new Truck()
+const v1 = new Car();
+const v2 = new Truck();
 
 let useVehicle = (vehicle: Vehicle) => {
-    vehicle.drive()
-    if ('loadCargo' in vehicle) {
-        vehicle.loadCargo(1000)
-    }
-}
+  vehicle.drive();
+  if ("loadCargo" in vehicle) {
+    vehicle.loadCargo(1000);
+  }
+};
 
-useVehicle(v1)
-useVehicle(v2)
+// useVehicle(v1)
+// useVehicle(v2)
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+type Animal = Bird | Horse;
+
+let moveAnimal = (animal: Animal) => {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+  }
+  console.log("Moving at speed: " + speed);
+};
+
+moveAnimal({ type: 'horse', runningSpeed: 23 });
